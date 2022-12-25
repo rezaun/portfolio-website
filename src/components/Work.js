bed6ad9af7a9a85bf501dacf6ecd3db5f77cf2db
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-12-26 03:22:33 +0600

    works section been done

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,11 +1,144 @@
 import React from 'react'
+import Afridi from '../assets/afridi-one.jpg'
 
 const Work = () => {
-  return (
-    <div name="work" className='w-full h-screen text-gray-300 bg-[#0a192f]'>
+    return (
+        <div name="work" className='w-full md:h-screen  text-gray-300 bg-[#0a192f]'>
+            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+                <div className='pb-8'>
+                    <p className='text-4xl inline font-bold border-b-4 text-gray-300 border-pink-600'>Work</p>
+                    <p className='py-4'>// Check out some of my recent work</p>
+                </div>
+                <div className='grid sm:grid-cols-3 md:grid-cols-3 gap-4'>
+                    <div style={{backgroundImage:`url(${Afridi})`}} className='shadow-lg shadow-[#040c16] group container rounded justify-center items-center mx-auto content-div'>
+                        {/* Hover effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                React Js Application
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href="/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                                </a>
 
-    </div>
-  )
+                                <a href="/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                        Code
+                                    </button>
+                                </a>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{backgroundImage:`url(${Afridi})`}} className='shadow-lg shadow-[#040c16] group container rounded-md justify-center items-center mx-auto content-div'>
+                        {/* Hover effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                React Js Application
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href="/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                                </a>
+
+                                <a href="/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                        Code
+                                    </button>
+                                </a>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{backgroundImage:`url(${Afridi})`}} className='shadow-lg shador-[#040c16] group container rounded justify-center items-center mx-auto content-div'>
+                        {/* Hover effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                React Js Application
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href="/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                                </a>
+
+                                <a href="/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                        Code
+                                    </button>
+                                </a>
+
+                            </div>
+                        </div>
+                    </div>
+                    {/* End single div */}
+                    <div style={{backgroundImage:`url(${Afridi})`}} className='shadow-lg shador-[#040c16] group container rounded justify-center items-center mx-auto content-div'>
+                        {/* Hover effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                React Js Application
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href="/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                                </a>
+
+                                <a href="/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                        Code
+                                    </button>
+                                </a>
+
+                            </div>
+                        </div>
+                    </div>
+                    {/* End single div */}
+                    <div style={{backgroundImage:`url(${Afridi})`}} className='shadow-lg shador-[#040c16] group container rounded justify-center items-center mx-auto content-div'>
+                        {/* Hover effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                React Js Application
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href="/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                                </a>
+
+                                <a href="/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                        Code
+                                    </button>
+                                </a>
+
+                            </div>
+                        </div>
+                    </div>
+                    {/* End single div */}
+                    <div style={{backgroundImage:`url(${Afridi})`}} className='shadow-lg shador-[#040c16] group container rounded justify-center items-center mx-auto content-div'>
+                        {/* Hover effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                React Js Application
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href="/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                                </a>
+
+                                <a href="/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                        Code
+                                    </button>
+                                </a>
+
+                            </div>
+                        </div>
+                    </div>
+                    {/* End single div */}
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default Work
