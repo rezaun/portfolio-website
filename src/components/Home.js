1d9a05f5f5566c3a1d632a6894d14e7ef653f6b4
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-12-26 03:48:15 +0600

    contact section been done

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,8 @@ const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
         {/* container  */}
-        <div className='mex-w-[1080px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <p className='text-pink-600'>Hi, my name is</p>
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p className='text-pink-600 font-bold'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Rezaun Kabir</h1>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>i'm a Full Stack Developer.</h2>
             <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a full-stack developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I'm focused on building responsive full-stack web applications.</p>
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Afridi from '../assets/afridi-one.jpg'
+import AR from '../assets/ar.jpg'
 
 const Work = () => {
     return (
@@ -10,7 +10,7 @@ const Work = () => {
                     <p className='py-4'>// Check out some of my recent work</p>
                 </div>
                 <div className='grid sm:grid-cols-3 md:grid-cols-3 gap-4'>
-                    <div style={{backgroundImage:`url(${Afridi})`}} className='shadow-lg shadow-[#040c16] group container rounded justify-center items-center mx-auto content-div'>
+                    <div style={{backgroundImage:`url(${AR})`}} className='shadow-lg shadow-[#040c16] group container rounded justify-center items-center mx-auto content-div'>
                         {/* Hover effects */}
                         <div className='opacity-0 group-hover:opacity-100'>
                             <span className='text-2xl font-bold text-white tracking-wider'>
@@ -30,7 +30,7 @@ const Work = () => {
                             </div>
                         </div>
                     </div>
-                    <div style={{backgroundImage:`url(${Afridi})`}} className='shadow-lg shadow-[#040c16] group container rounded-md justify-center items-center mx-auto content-div'>
+                    <div style={{backgroundImage:`url(${AR})`}} className='shadow-lg shadow-[#040c16] group container rounded-md justify-center items-center mx-auto content-div'>
                         {/* Hover effects */}
                         <div className='opacity-0 group-hover:opacity-100'>
                             <span className='text-2xl font-bold text-white tracking-wider'>
@@ -50,28 +50,7 @@ const Work = () => {
                             </div>
                         </div>
                     </div>
-                    <div style={{backgroundImage:`url(${Afridi})`}} className='shadow-lg shador-[#040c16] group container rounded justify-center items-center mx-auto content-div'>
-                        {/* Hover effects */}
-                        <div className='opacity-0 group-hover:opacity-100'>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                                React Js Application
-                            </span>
-                            <div className='pt-8 text-center'>
-                                <a href="/">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                                </a>
-
-                                <a href="/">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                        Code
-                                    </button>
-                                </a>
-
-                            </div>
-                        </div>
-                    </div>
-                    {/* End single div */}
-                    <div style={{backgroundImage:`url(${Afridi})`}} className='shadow-lg shador-[#040c16] group container rounded justify-center items-center mx-auto content-div'>
+                    <div style={{backgroundImage:`url(${AR})`}} className='shadow-lg shador-[#040c16] group container rounded justify-center items-center mx-auto content-div'>
                         {/* Hover effects */}
                         <div className='opacity-0 group-hover:opacity-100'>
                             <span className='text-2xl font-bold text-white tracking-wider'>
@@ -92,7 +71,7 @@ const Work = () => {
                         </div>
                     </div>
                     {/* End single div */}
-                    <div style={{backgroundImage:`url(${Afridi})`}} className='shadow-lg shador-[#040c16] group container rounded justify-center items-center mx-auto content-div'>
+                    <div style={{backgroundImage:`url(${AR})`}} className='shadow-lg shador-[#040c16] group container rounded justify-center items-center mx-auto content-div'>
                         {/* Hover effects */}
                         <div className='opacity-0 group-hover:opacity-100'>
                             <span className='text-2xl font-bold text-white tracking-wider'>
@@ -113,7 +92,28 @@ const Work = () => {
                         </div>
                     </div>
                     {/* End single div */}
-                    <div style={{backgroundImage:`url(${Afridi})`}} className='shadow-lg shador-[#040c16] group container rounded justify-center items-center mx-auto content-div'>
+                    <div style={{backgroundImage:`url(${AR})`}} className='shadow-lg shador-[#040c16] group container rounded justify-center items-center mx-auto content-div'>
+                        {/* Hover effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                React Js Application
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href="/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                                </a>
+
+                                <a href="/">
+                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                        Code
+                                    </button>
+                                </a>
+
+                            </div>
+                        </div>
+                    </div>
+                    {/* End single div */}
+                    <div style={{backgroundImage:`url(${AR})`}} className='shadow-lg shador-[#040c16] group container rounded justify-center items-center mx-auto content-div'>
                         {/* Hover effects */}
                         <div className='opacity-0 group-hover:opacity-100'>
                             <span className='text-2xl font-bold text-white tracking-wider'>
