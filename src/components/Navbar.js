be7de400d0699ff49069aae45053349128b42413
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-12-25 18:21:09 +0600

    social icon added in the here section has been done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
-import { FaBars, FaTimes } from 'react-icons/fa'
+import { FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook } from 'react-icons/fa'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
+import {HiOutlineMail} from 'react-icons/hi'
 import Logo from '../assets/logo1.png'
 
 const Navbar = () => {
@@ -31,6 +33,27 @@ const Navbar = () => {
                 <li className='py-6 text-4xl'>Work</li>
                 <li className='py-6 text-4xl'>Contacts</li>
             </ul>
+            {/* social icons     */}
+            <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+                <ul>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+                        <a className='flex justify-between items-center w-full text-gray-300' href="https://www.facebook.com/" target="_blank">
+                            LinkedIn <FaLinkedin size={30}/></a>
+                    </li>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333]'>
+                        <a className='flex justify-between items-center w-full text-gray-300' href="https://www.facebook.com/" target="_blank">
+                            GitHub <FaGithub size={30}/></a>
+                    </li>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+                        <a className='flex justify-between items-center w-full text-gray-300' href="https://www.facebook.com/" target="_blank">
+                            Email <HiOutlineMail size={30}/></a>
+                    </li>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+                        <a className='flex justify-between items-center w-full text-gray-300' href="https://www.facebook.com/" target="_blank">
+                            Resume <BsFillPersonLinesFill size={30}/></a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
