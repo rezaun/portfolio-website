c333e2a5e7d0d805c1c32363661a05e8035f7aa5
Rezaun Kabir <rezaun.kabir@gmail.com>
2022-12-25 20:09:35 +0600

    about section been done

---
+++ src/components/About.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const ABout = () => {
+  return (
+    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+        <div className='flex flex-col justify-center items-center w-full h-full'>
+            <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
+                <div className='sm:text-right pb-8 pl-4'>
+                    <p className='text-4xl font-bold inline border-b-4 border-pink-600'>About</p>
+                </div>
+                <div></div>
+            </div>
+            <div className='max--w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+                <div className='sm:text-right text-4xl font-bold'>
+                    <h1>Hi, I'm Clint, nice to meet you. Please take a look around.</h1>
+                </div>
+                <div>
+                    <p>I am passionate about buildings excellent software that improves the lives of those around me. I specialize in creating software for clients ranging from individuals and small-business all the way to large enterprise corporations. what would you do if you had a software expert available at your fingertips</p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default ABout
